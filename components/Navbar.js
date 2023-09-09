2034e248972c52041fdffff24b20ab6223a19974
AtharvaMaskar <107984209+AtharvaMaskar26@users.noreply.github.com>
2023-09-09 11:35:52 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,16 +8,11 @@ export default function Navbar() {
   const { account } = useMoralis();
 
   return (
-    <nav className="pt-5 pb-5 pr-10 pl-10 h-70 w-full bg-white fixed top-0 p-0 font-bold border-b-2 border-solid border-black z-99">
+    <nav className="pt-5 pb-5 pr-10 pl-10 h-70 w-full bg-white fixed top-0 p-0 font-bold border-b-2 border-solid border-slate-700 bg-[#03001C]  z-99">
       <ul className="list-none m-0 p-0 flex items-center justify-between h-full">
         <li className="rounded">
           <Link href="/">
-            <img
-              className="rounded-t-lg"
-              src="logo-full-black.svg"
-              alt=""
-              width={220}
-            />
+            <h1 className="font-bold text-3xl text--300 text-[#B6EADA]">Peer Web</h1>
           </Link>
         </li>
 
@@ -28,9 +23,9 @@ export default function Navbar() {
                 <div className="flex space-x-2 justify-center">
                   <button
                     type="button"
-                    className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                    className="text-[#5B8FB9] bg-white py-2 px-4 rounded-xl hover:bg-[#B6EADA]"
                   >
-                    Write Blog
+                    Write Post
                   </button>
                 </div>
               </Link>
