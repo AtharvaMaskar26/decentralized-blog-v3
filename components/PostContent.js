8054d7a0e4e2d52c821e9a2c72ffd8d8bddfda30
AtharvaMaskar <107984209+AtharvaMaskar26@users.noreply.github.com>
2023-09-09 11:36:13 +0530

    Update PostContent.js

--- components/PostContent.js
+++ components/PostContent.js
@@ -1,14 +1,15 @@
 import ReactMarkdown from "react-markdown";
 
 export default function PostContent({ post }) {
+  console.log(post.metadata.name);
   return (
-    <div className="flex justify-center p-2">
+    <div className="flex justify-center mt-16 bg-[#03001C]">
       <div className="p-6">
-        <h5 className="text-gray-900 text-xl font-medium mb-2">
+        <h5 className="text-xl text-white font-medium mb-8">
           {post.metadata.name}
         </h5>
-        <span className="text-sm">Written by {post.profile.name}</span>
-        <ReactMarkdown className="markdown">
+        <span className="text-xl font-bold bg-white rounded-xl px-4 py-2">Written by {post.profile.name}</span>
+        <ReactMarkdown className="markdown text-white">
           {post.metadata.content}
         </ReactMarkdown>
       </div>
